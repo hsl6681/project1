960c0ed3d5952ada68bec2b7804e86cf19f42a78
LeeHaeDam <time9410668917@gamil.com>
2022-11-15 20:22:05 +0900

    first

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+import certifi
+
+ca = certifi.where()
+
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://hsl6681:goeka9366^^@starta.ycijw1q.mongodb.net/Starta?retryWrites=true&w=majority',
+                     tlsCAFile=ca)
+db = client.dbsparta
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    name_receive = request.form["name_give"]
+    comment_receive = request.form["comment_give"]
+
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive
+    }
+
+    db.homework.insert_one(doc)
+    return jsonify({'msg': '응원 완료!'})
+
+
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    comment_list = list(db.homework.find({}, {'_id': False}))
+    return jsonify({'comments': comment_list})
+
+
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
